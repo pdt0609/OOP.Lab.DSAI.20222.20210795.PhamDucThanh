a8f419a3f1833b60aef73655467e6757fa7f9a83
pdt0609 <phamthanh060903@gmail.com>
2023-04-28 08:46:36 +0700

    update

--- AIMS/src/Lab02/DigitalVideoDisc.java
+++ AIMS/src/Lab02/DigitalVideoDisc.java
@@ -15,11 +15,55 @@ public class DigitalVideoDisc {
 	public String getDirector() {
 		return director;
 	}
+
 	public int getLength() {
 		return length;
 	}
-	public float getCost() {
+	public DigitalVideoDisc(String title, String category, float cost) {
+		super();
+		this.title = title;
+		this.category = category;
+		this.cost = cost;
+	}
+	public DigitalVideoDisc(String title) {
+		super();
+		this.title = title;
+	}
+		
+	public DigitalVideoDisc(String title, String category, String director, float cost) {
+			super();
+			this.title = title;
+			this.category = category;
+			this.director = director;
+			this.cost = cost;	
+		}
+	
+		
+		public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+		super();
+		this.title = title;
+		this.category = category;
+		this.director = director;
+		this.length = length;
+		this.cost = cost;
+	}
+		public float getCost() {
 		return cost;
 	}
-	DigitalVideoDisc dvd1 = new DigitalVideoDisc();
+		public void setTitle(String title) {
+			this.title = title;
+		}
+		public void setCategory(String category) {
+			this.category = category;
+		}
+		public void setDirector(String director) {
+			this.director = director;
+		}
+		public void setLength(int length) {
+			this.length = length;
+		}
+		public void setCost(float cost) {
+			this.cost = cost;
+		}
+
 }
---
+++ AIMS/src/Lab02/test.java
@@ -0,0 +1,9 @@
+package Lab02;
+
+public class test {
+protected void finalize() throws Throwable{
+	
+}
+//ask garbage  collector to collect(because the gc is only run when memory low)
+System.gc();qws
+}
